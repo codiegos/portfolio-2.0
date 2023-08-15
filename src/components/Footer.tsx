5bdd08dad6d3c6263326d1b4b20b58276e3380ec
Diego Rivera <diegosebsk@gmail.com>
2023-08-15 02:50:45 -0400

    add: social medias array

---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <footer className='bg-tertiary py-12'>
+      <p className='text-center italic '>
+        Designed & Developed by Diego Sebastián Rivera.
+      </p>
+    </footer>
+  )
+}
+export default Footer
