336b8081d896d0276ab0654211ee98af688e1d53
Diego Rivera <diegosebsk@gmail.com>
2023-08-16 17:21:15 -0400

    refactor: properly wrapped components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,3 @@
-'use client'
-import { MenuProvider } from '@/contexts/menu-context'
-import Header from '@/components/Header'
 import Intro from '@/components/Intro'
 import About from '@/components/About'
 import Projects from '../components/Projects'
@@ -9,23 +6,14 @@ import Footer from '@/components/Footer'
 
 export default function Home() {
   return (
-    <MenuProvider>
-      <Header />
-      <main className='h-screen w-full overflow-x-hidden scroll-smooth duration-1000'>
-        <div id='home'>
-          <Intro />
-        </div>
-        <div id='about' className='lg:bg-tertiary/70'>
-          <About />
-        </div>
-        <div id='projects' className='bg-tertiary/70'>
-          <Projects />
-        </div>
-        <div id='contact' className='bg-tertiary/70'>
-          <Contact />
-        </div>
+    <>
+      <main className=' '>
+        <Intro />
+        <About />
+        <Projects />
+        <Contact />
         <Footer />
       </main>
-    </MenuProvider>
+    </>
   )
 }
